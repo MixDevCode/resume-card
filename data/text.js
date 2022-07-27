62cd82ed781787b440b5214fd05176e432357c3e
Mix <txtbmx_teen@live.com>
2022-07-27 15:47:58 -0300

    first commit

---
+++ data/text.js
@@ -0,0 +1,12 @@
+const datatext = {
+    "en":
+    {
+        "work": "Designer & Developer",
+        "description": "Hi, im Lucas. I am a developer and a constant contributor to open source, i'm currently a student of a programming career, but i'm not just that, i'm constantly developing things that catch my attention. \n\nAs of today, i am interested in programming discord bots, but i have also developed complete CMS or static websites.\n\n"
+    },
+    "es":
+    {
+        "work": "Diseñador & Desarrollador",
+        "description": "Hola, soy Lucas. Soy un desarrollador y un contribuyente a la librería open source, actualmente estoy estudiando una carrera de programación, pero no solo eso, siempre estoy desarrollando cosas que me atraen. \n\nAl día de hoy, estoy interesado en la programación de bots, pero tambien creé CMS completas o sitios web estáticos.\n\n"
+    }
+};
