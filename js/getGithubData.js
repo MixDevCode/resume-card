4d2004e59ee5db077a2032e9f290a573398b6ed6
Mix <txtbmx_teen@live.com>
2022-07-30 12:09:39 -0300

    heh

--- js/getGithubData.js
+++
@@ -1,8 +0,0 @@
-import { get } from './axios.min.js';
-
-export default async function () {
-    const { data: reposData } = await get('https://api.github.com/users/MixDevCode/repos');
-    reposData.forEach(element => {
-        console.log(element);
-    });
-}
